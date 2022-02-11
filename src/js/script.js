4f3d9ab127a9688a73ddc581c80b570811c2b3e5
azyabon <andrey.azyabin@bk.ru>
2022-02-11 17:37:24 +0700

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -11,11 +11,26 @@ function changeTitle(title, e) {
         for (let i = 0; i < AllLi.length; i++) {
             AllLi[i].classList.remove("active");
         }
-        li.classList.toggle("active");
+        if (li.id === "link") {
+            li.classList.toggle("active");
+        } else {
+            li.parentElement.classList.toggle("active");
+        }
     }
 }
 
 
 function openMenu() {
     document.getElementById("sidebar").classList.toggle("sidebar-active");
+    if (!document.getElementById("sidebar").classList.contains("sidebar-active")) {
+        for (let i = 0; i < document.getElementsByClassName("sidebar-menu__link").length; i++) {
+            document.getElementsByClassName("sidebar-menu__link")[i].style.display = "none";
+        }
+        document.getElementsByClassName("sidebar-logo__title")[0].style.display = "none";
+    } else {
+        for (let i = 0; i < document.getElementsByClassName("sidebar-menu__link").length; i++) {
+            document.getElementsByClassName("sidebar-menu__link")[i].style.display = "block";
+        }
+        document.getElementsByClassName("sidebar-logo__title")[0].style.display = "block";
+    }
 }
